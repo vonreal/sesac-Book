3f241853b9b158159e50f263534642bcc2350a8d
jiun <vonreal20@gmail.com>
2022-07-20 18:19:44 +0900

    ♻️ [독서 다이어리] ui 개선

--- Book/BookInfo.swift
+++ Book/BookInfo.swift
@@ -11,14 +11,14 @@ import UIKit
 struct BookInfo {
     
     let books: [Book] = [
-        Book(title: "헤어질 결심 각본", coverImage: "https://image.aladin.co.kr/product/29820/33/cover500/8932474753_1.jpg", recordDate: "08.05", backgroundColor: .darkGray),
-        Book(title: "알싸한 기린의 세계", coverImage: "https://image.aladin.co.kr/product/29568/39/cover500/k382837451_2.jpg", recordDate: "07.18", backgroundColor: .brown),
-        Book(title: "파친코 1", coverImage: "https://image.aladin.co.kr/product/29496/39/cover500/k202838509_2.jpg", recordDate: "07.27", backgroundColor: .brown),
-        Book(title: "유럽 도시 기행 2", coverImage: "https://image.aladin.co.kr/product/29689/64/cover500/8965137705_1.jpg", recordDate: "07.09", backgroundColor: .darkGray),
-        Book(title: "역행자", coverImage: "https://image.aladin.co.kr/product/29521/63/cover500/8901260719_3.jpg", recordDate: "06.03", backgroundColor: .darkGray),
-        Book(title: "믿음에 대하여", coverImage: "https://image.aladin.co.kr/product/29808/73/cover500/8954699804_2.jpg", recordDate: "07.20", backgroundColor: .brown),
-        Book(title: "계속 가보겠습니다", coverImage: "https://image.aladin.co.kr/product/29792/65/cover500/k282838912_1.jpg", recordDate: "07.22", backgroundColor: .brown),
-        Book(title: "마법천자문 53", coverImage: "https://image.aladin.co.kr/product/29781/62/cover500/895090313x_1.jpg", recordDate: "07.12", backgroundColor: .darkGray)
+        Book(title: "헤어질 결심 각본", coverImage: "https://image.aladin.co.kr/product/29820/33/cover500/8932474753_1.jpg", recordDate: "8.5", backgroundColor: .darkGray),
+        Book(title: "알싸한 기린의 세계", coverImage: "https://image.aladin.co.kr/product/29568/39/cover500/k382837451_2.jpg", recordDate: "7.18", backgroundColor: .brown),
+        Book(title: "파친코 1", coverImage: "https://image.aladin.co.kr/product/29496/39/cover500/k202838509_2.jpg", recordDate: "7.27", backgroundColor: .brown),
+        Book(title: "유럽 도시 기행 2", coverImage: "https://image.aladin.co.kr/product/29689/64/cover500/8965137705_1.jpg", recordDate: "7.9", backgroundColor: .darkGray),
+        Book(title: "역행자", coverImage: "https://image.aladin.co.kr/product/29521/63/cover500/8901260719_3.jpg", recordDate: "6.3", backgroundColor: .darkGray),
+        Book(title: "믿음에 대하여", coverImage: "https://image.aladin.co.kr/product/29808/73/cover500/8954699804_2.jpg", recordDate: "7.20", backgroundColor: .brown),
+        Book(title: "계속 가보겠습니다", coverImage: "https://image.aladin.co.kr/product/29792/65/cover500/k282838912_1.jpg", recordDate: "7.22", backgroundColor: .brown),
+        Book(title: "마법천자문 53", coverImage: "https://image.aladin.co.kr/product/29781/62/cover500/895090313x_1.jpg", recordDate: "7.12", backgroundColor: .darkGray)
         
     ]
 }
